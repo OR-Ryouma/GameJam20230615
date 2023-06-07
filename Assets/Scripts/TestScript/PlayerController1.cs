827fc85aae44041edb9774dc5148fe6c6695e416
RAK_kanato <tennbu1@gmail.com>
2023-06-07 16:30:55 +0900

    プレイヤーの動作を2種類作成

--- Assets/Scripts/TestScript/PlayerController1.cs
+++ Assets/Scripts/TestScript/PlayerController1.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 // アタッチしたGameObjectにRigidbodyがアタッチされていない場合、アタッチする
 [RequireComponent(typeof(Rigidbody))]
 
-public class PlayerMove : MonoBehaviour
+public class PlayerController1 : MonoBehaviour
 {
     // 移動速度を設定（Inspectorタブからも値を変更できるようにする）
     [SerializeField]
@@ -22,13 +22,14 @@ public class PlayerMove : MonoBehaviour
         _rigidBody = GetComponent<Rigidbody>();
     }
 
-    //一秒間に一定の回数呼ばれる(物理演算(Rigidbody)を使った移動はFixedUpdateで行った方が良いらしい)
+    //一秒間に一定の回数呼ばれる
+    //物理挙動の更新が時間進行単位で行われているため、フレーム更新は関係ないため。FixedUpdateは物理挙動更新前に呼び出されるため、その中で行ったどのような変更も直接反映される
     private void FixedUpdate()
     {
         // 左右のキーの入力を取得
         float x = Input.GetAxisRaw("Horizontal");
         // 前後のキーの入力を取得
-        //float z = Input.GetAxisRaw("Vertical");
+        //float z = Input.GetAxisRaw+("Vertical");
 
         // 上下左右キーを入力して得た値にspeedをかけた値をAddForceに設定して移動させる
         //（ForceMode.Impulseは瞬間的に力を加えるということ）
